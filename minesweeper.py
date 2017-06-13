9617b4522a2f716af5f658a7028a035485854b55
Patrick Mannion <ptm@Patricks-MacBook-Pro.local>
2017-06-13 09:08:09 -0400

    added input with basic validity check

--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,13 @@
 import random
+import pdb
 
 MINE_DENSITY = 0.20 # based on online game 16x30 board with 99 mines
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 10
 
 def create_board(size):
+    '''create board with mines and adjacency counts, plus full mask'''
+
     width, height = size
     area = width * height
 
@@ -67,9 +70,33 @@ def create_board(size):
 
     return (board,mask)
 
-def get_input():
-    # get column and row number
-    pass
+def get_input(size):
+    ''' get user input of column and row number '''
+
+    width, height = size
+
+    print "Select a block to click (format: row,column): "
+    user_input = raw_input()
+
+    try:
+        r,c = user_input.split(",")
+        r = int(r)
+        c = int(c)
+
+        if r >= height:
+            print "Maximum row value is %i. Try again:" % (height-1)
+            move = get_input(size)
+        elif c >= width:
+            print "Maximum column value is %i. Try again:" % (width-1)
+            move = get_input(size)
+        else:
+            move = (r,c)
+
+    except ValueError:
+         print "Please use the format specified, e.g., '0,0'"
+         move = get_input(size)
+
+    return move
 
 def update_board(move):
     # if coordinates are a mine, game over
@@ -117,6 +144,7 @@ if __name__ == "__main__":
     size = (BOARD_WIDTH,BOARD_HEIGHT)
     board,mask = create_board(size)
     print_board(board,mask)
+    get_input(size)
 
     # while gameplay == True:
     #     print_board(board)
