536265349a66015f232be6355d421bb0143989c8
Patrick Mannion <ptm@Patricks-MacBook-Pro.local>
2017-06-13 19:27:43 -0400

    code cleanup and comments

--- minesweeper.py
+++ minesweeper.py
@@ -12,19 +12,6 @@ BOARD_HEIGHT = 10
 
 START_TIME = time.time()
 
-def create_mask(value):
-    ''' create mask as list of lists (list of rows) of repeated value input '''
-
-    if type(value) != int:
-        raise ValueError('Mask value must be int.')
-
-    mask = []
-    for r in range(0,BOARD_HEIGHT):
-        row = [value] * BOARD_WIDTH
-        mask.append(row)
-
-    return mask
-
 def valid_space(r,c):
     ''' check if a set of coordinates is valid on the board '''
 
@@ -55,11 +42,15 @@ def get_neighbors(r,c):
 def create_board():
     ''' create board with mines and adjacency counts '''
 
-    # create board as list of lists (list of rows)
+    # create blank board as list of rows
     board = []
+    mask = []
     for r in range(0,BOARD_HEIGHT):
-        row = ["0"] * BOARD_WIDTH
-        board.append(row)
+        board_row = ["0"] * BOARD_WIDTH
+        board.append(board_row)
+
+        mask_row = [1] * BOARD_WIDTH
+        mask.append(mask_row)
 
     # place mines
     mine_count = 0
@@ -68,42 +59,35 @@ def create_board():
         for c in range(0,BOARD_WIDTH):
             v = random.randint(1,1/MINE_DENSITY)
             if v == 1:
-                has_mine = True
+                board[r][c] = "*"
                 mine_count += 1
                 mine_locations.append((r,c))
-            else:
-                has_mine = False
-
-            if has_mine:
-                board[r][c] = "*"
 
     # count adjacent mines
     for r in range(0,BOARD_HEIGHT):
         for c in range(0,BOARD_WIDTH):
+            # skip mines
             if board[r][c] == "*":
                 continue
 
-            # enumerate neighbor coords clockwise from top
-            valid_neighbors = get_neighbors(r,c)
-
-            # count neighboring mines and use to update block
+            # count neighboring mines and use to update cell
             count = 0
-            for nr,nc in valid_neighbors:
+            for nr,nc in get_neighbors(r,c):
                     if board[nr][nc] == "*":
                         count += 1
-
             board[r][c] = str(count)
 
-    return (board,mine_count,mine_locations)
+    return (board,mask,mine_count,mine_locations)
 
 def get_input():
     ''' get user input of column and row number '''
 
-    print "\nEnter row,col to click, f#,# to flag or u#,# to unflag."
+    print "\nEnter row,col to click, f#,# to flag mine or u#,# to unflag."
     user_input = raw_input()
 
-    move_type = 'click' # set default
+    move_type = 'click' # set as default
 
+    # override default move type and strip input to coords
     if user_input[0] == 'f':
         move_type = 'flag'
         user_input = user_input[1:]
@@ -111,21 +95,19 @@ def get_input():
         move_type = 'unflag'
         user_input = user_input[1:]
 
+    # check for valid coords of move
     try:
         r,c = user_input.split(",")
         r = int(r)
         c = int(c)
-
         if mask[r][c] == 0:
             print "This block has already been clicked. Try again:"
             move = get_input()
-
         if not valid_space(r,c):
             print "Your move is not on the board. Try again:"
             move = get_input()
         else:
             move = (r,c)
-
     except ValueError:
          print "Please use the format specified, e.g., '0,0'"
          move = get_input()
@@ -135,20 +117,19 @@ def get_input():
 def recursive_clear(r,c,mask):
     ''' remove mask from cell and adjacent cells as needed on click '''
 
+    # base case - cell already cleared
     if mask[r][c] == 0:
-        # base case - cell already cleared
         return mask
 
+    # base case - number cell at edge of area to clear
     if int(board[r][c]) >= 1:
-        # base case - number cell at edge of area to clear
         mask[r][c] = 0
         return mask
 
+    # recursive case - blank cell, need to clear neighbors
     elif int(board[r][c]) == 0:
-        # recursive case - blank cell, need to clear neighbors
         mask[r][c] = 0
         valid_neighbors = get_neighbors(r,c)
-
         for nr,nc in valid_neighbors:
             mask = recursive_clear(nr,nc,mask)
 
@@ -157,20 +138,21 @@ def recursive_clear(r,c,mask):
 def update_mask(board,mask,move,move_type,mine_count):
     r,c = move
 
+    # normal click moves
     if move_type == 'click':
+        # game over
         if board[r][c] == "*":
-            # game over
             os.system('clear')
-
             for mr,mc in mine_locations:
                 mask[mr][mc] = 0
             print_board(board,mask,mine_count)
-
             print colored('Game over!','red')
             quit()
+        # normal move - clear cell(s)
         else:
-            # otherwise you clicked a number, clear the cell
             mask = recursive_clear(r,c,mask)
+
+    # flag and unflag moves
     elif move_type == 'flag':
         mask[r][c] = 2
         mine_count -= 1
@@ -217,7 +199,7 @@ def print_board(board,mask,mine_count):
 
         # print row contents
         for c in range(0,BOARD_WIDTH):
-            # only print row if mask is 0, not 1
+            # only print cell values if mask is 0
             if mask[r][c] == 0:
                 value = board[r][c]
                 block = '  %s  ' % value
@@ -226,6 +208,7 @@ def print_board(board,mask,mine_count):
                 else:
                     print colored(block,'green'),
 
+            # handle masked (blank) and flagged cells
             elif mask[r][c] == 1:
                 block = "     "
                 print block,
@@ -236,15 +219,17 @@ def print_board(board,mask,mine_count):
 
 if __name__ == "__main__":
 
+    # handle custom board parameters
     if len(sys.argv) > 2:
         BOARD_WIDTH = int(sys.argv[1])
         BOARD_HEIGHT = int(sys.argv[2])
     if len(sys.argv) > 3:
         MINE_DENSITY = float(sys.argv[3])
 
-    board,mine_count,mine_locations = create_board()
-    mask = create_mask(1)
+    # initialize board
+    board,mask,mine_count,mine_locations = create_board()
 
+    # run game loop
     while True:
         # clear console window
         os.system('clear')
